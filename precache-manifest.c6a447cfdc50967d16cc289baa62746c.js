ec0549d8731a2dbb21a5983c64cdd0c62f08b1c8
StephaneGcrd <stephane.gcrd@gmail.com>
2018-11-22 22:17:14 +0100

    Updates

---
+++ precache-manifest.c6a447cfdc50967d16cc289baa62746c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tweetepi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dbebcbb88766d658736a",
+    "url": "/Tweetepi/static/js/runtime~main.dbebcbb8.js"
+  },
+  {
+    "revision": "87d019bf6682f3436162",
+    "url": "/Tweetepi/static/js/main.87d019bf.chunk.js"
+  },
+  {
+    "revision": "0209cce71cdfd2ccb6ed",
+    "url": "/Tweetepi/static/js/1.0209cce7.chunk.js"
+  },
+  {
+    "revision": "87d019bf6682f3436162",
+    "url": "/Tweetepi/static/css/main.39c3a827.chunk.css"
+  },
+  {
+    "revision": "cc67996b00441c919a48d4ff43d03e72",
+    "url": "/Tweetepi/index.html"
+  }
+];
